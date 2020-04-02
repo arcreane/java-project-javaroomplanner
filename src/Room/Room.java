d5b747d9d64ae9be7d180ebc73f0ed106f140fb8
Laure C <compointlaure@gmail.com>
2020-04-02 19:22:20 +0200

    organiser projet en module package ( user , room , main )

---
+++ src/Room/Room.java
@@ -0,0 +1,11 @@
+package Room;
+
+import User.User;
+
+public class Room {
+	public Object Material;
+	public Object Available;
+	public Object Slot;
+	public User Users;
+	// public Object Room-size;
+}
