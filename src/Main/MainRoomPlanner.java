6b4f8cbe962eb3f68df00285359e8f9ce5438c74
Laure C <compointlaure@gmail.com>
2020-04-07 16:22:25 +0200

    add partie user inscription debut et deplacer la partie debut login dans la methode LoginUser()

--- src/Main/MainRoomPlanner.java
+++ src/Main/MainRoomPlanner.java
@@ -1,12 +1,17 @@
 package Main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import User.Collaborator;
+import User.User;
 
 
 public class MainRoomPlanner {
 	public static void main(String[] args) {
+		ArrayList<User> user = new ArrayList<>();
 		System.out.println("Hello, bienvenue dans notre syteme de gestion d'utilisateur");
-		System.out.println("Veuillez vous connecter ( taper 1 ) ou vous inscricre ( taper 2 ) pour acceder a la gestion de vos salles");
+		System.out.println("Veuillez vous connecter ( taper 1 ) ou vous inscricre ( taper 2 ) pour acceder a la gestion de vos salles.");
 		Scanner scanuser = new Scanner(System.in);
 		while( scanuser.hasNextInt() == false ) {
 			System.out.println("Choisie soit 1 pour la connexion ou 2 si tu n'est pas encore inscrit");
@@ -16,34 +21,13 @@ public class MainRoomPlanner {
 		 switch(answer) {
 		 case 1:
 			System.out.println("Vous aller pouvoir vous connecter");
-			 Scanner input = new Scanner(System.in);
-		        System.out.print("UserName: ");
-		        String username= input.nextLine();
-		        System.out.print("Password: ");
-		        String password= input.nextLine();
-		        System.out.println("Username: " + username);
-		        System.out.println("Password: " + password);
-		        while(!username.equals("test") && !password.equals("test") ){
-					System.out.println("Veuillez recommencer ");
-					 System.out.print("UserName: ");
-					  username= input.nextLine();
-					 System.out.print("Password: ");
-					  password= input.nextLine();
-				}
-		        if (username.equals("test")){
-		            if (password.equals("test")){
-		                System.out.println("Vous etez connecter :)");
-		            }
-		            else{
-		                System.out.println("Votre tentative de connexion a echouer :(");
-		               
-		            }
-		        }
+			User userlogin = new User();
+			userlogin.LoginUser();
 			
 		    break;
 		  case 2:
 			System.out.println("Vous aller pouvoir vous s'incricre");
-			  
+			 user.add(Collaborator.createnewusercollaborateur());
 		    break;
 		
 		  default:
