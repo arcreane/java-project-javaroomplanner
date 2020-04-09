5df14d21574d3538d3c9e107e66780a9d101c4c6
MacInLife <marie.ange.coco@gmail.com>
2020-04-09 13:30:07 +0200

    change text main

--- src/Main/MainRoomPlanner.java
+++ src/Main/MainRoomPlanner.java
@@ -10,23 +10,23 @@ import User.User;
 public class MainRoomPlanner {
 	public static void main(String[] args) {
 		ArrayList<User> user = new ArrayList<>();
-		System.out.println("Hello, bienvenue dans notre syteme de gestion d'utilisateur");
-		System.out.println("Veuillez vous connecter ( taper 1 ) ou vous inscricre ( taper 2 ) pour acceder a la gestion de vos salles.");
+		System.out.println("Hello, bienvenue dans RoomPlanner");
+		System.out.println("Veuillez vous connecter ( taper 1 ) ou vous inscricre ( taper 2 )");
 		Scanner scanuser = new Scanner(System.in);
 		while( scanuser.hasNextInt() == false ) {
-			System.out.println("Choisie soit 1 pour la connexion ou 2 si tu n'est pas encore inscrit");
+			System.out.println("Choisi 1 pour te connecter ou 2 pour t'inscrire");
 			scanuser.next();
 		}	
 		 int answer = scanuser.nextInt();
 		 switch(answer) {
 		 case 1:
-			System.out.println("Vous aller pouvoir vous connecter");
+			System.out.println("Vous connecter :");
 			User userlogin = new User();
 			userlogin.LoginUser();
 			
 		    break;
 		  case 2:
-			System.out.println("Vous aller pouvoir vous s'incricre");
+			System.out.println("Vous incricre");
 			 user.add(Collaborator.createnewusercollaborateur());
 		    break;
 		
