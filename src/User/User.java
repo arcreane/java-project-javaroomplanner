9c617d7e6a8b29b790f1c5106cd5fd39e3a39f51
Laure C <compointlaure@gmail.com>
2020-04-09 17:17:55 +0200

    delete loginuser (partie inutile ) et ajoue de role user register

--- src/User/User.java
+++ src/User/User.java
@@ -29,24 +29,7 @@ public abstract class User {
 		return Mail;
 	}
 
-	public void LoginUser() {
-
-		System.out.print("UserName: ");
-		String username = input.nextLine();
-		System.out.print("Password: ");
-		String password = input.nextLine();
-		System.out.println("Username: " + username);
-		System.out.println("Password: " + password);
-
-		if (username.equals("test")) {
-			if (password.equals("test")) {
-				System.out.println("Vous etez connecter :)");
-			} else {
-				System.out.println("Votre tentative de connexion a echouer :(");
-
-			}
-		}
-	}
+	
 
 	static public User RegisterUser() {
 		System.out.println(
@@ -57,16 +40,9 @@ public abstract class User {
 		String Mail = GetUserInput("Mail: ");
 		String Password = GetUserInput("Password: ");
 		String ConfirmPassword = GetUserInput("Confirm-Password: ");
-//
-//		if (Password.equals(ConfirmPassword)) {
-//			System.out.println(
-//					"Vos information d'inscription sont les suivante : - votre nom : " + Name + ", - votre prenom : "
-//							+ Username + ", - votre email:  " + Mail + " , - votre password:  " + Password);
-//			System.out.println("Vous etez desormais connecter");
-//		} else {
-//			System.out.println("Erreur, avec vos information, les mots de passe ne correspond pas");
-//			System.out.println("Vous n'etez pas encore connecter");
-//		}
+
+		
+		
 
 		String Role = GetUserInput("Quel est votre role \\n"
 				+ "1 Collaborateur,\n"
@@ -75,9 +51,17 @@ public abstract class User {
 		User user = null;
 		switch(Role)
 		{
-		case "1":
-			user = new Collaborator(Name, Username, Mail, Password, ConfirmPassword);
-			break;
+			case "1":
+				user = new Collaborator(Name, Username, Mail, Password, ConfirmPassword);
+				break;
+				
+			case "2":
+				user = new Guest(Name, Username, Mail, Password, ConfirmPassword);
+				break;
+			
+			case "3":
+				user = new Manager(Name, Username, Mail, Password, ConfirmPassword);
+				break;
 		}
 		return user;
 	}
@@ -94,6 +78,7 @@ public abstract class User {
 		// 
 		return Password.equals(mdp);
 	}
+	
 	
 	public abstract void StartWorking();
 }
